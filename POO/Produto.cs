fb3b395d36f3f958571cda459732af39dd0b78e3
fic <fic@NOTE04-S28>
2025-03-26 21:54:00 -0300

    aula 26/03

---
+++ POO/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    class Produto
+    {
+        public string nome;
+        public double preco;
+        public int qtdEestoque;
+        public double CalcularTotal()
+        {
+            double calcularTotal = qtdEestoque * preco;
+            return calcularTotal;
+        }
+    }
+}
--- POO/Program.cs
+++ POO/Program.cs
@@ -13,7 +13,7 @@
 //}
 
 //listasDeNomes.Remove("leinaD");
-
+///
 //// POO - Programacao Orientada a Objetos
 
 //// Objetivo - Aproximar a programcao do mundo real
@@ -25,6 +25,23 @@
 //// Atributos (caracteriscas) - metodos (acoes)
 
 //// Objeto - Instancia de uma classe
+
+//// ferramentas - formas de trabalhar com POO
+
+//// Emcapsulamento e Heranca
+
+//// Emcapsulamento - Esconder comportamentos e atributos
+//// Evitar que os dados sejam acessados diretamente
+
+//// public - Esse atributo/metodo/classes podem ser acessadas por qualquer um.
+
+//// private - Esse atributo/metodo/classes so pode ser acessado dentro dele mesmo.
+
+//// Abstracao e polimorfismo
+
+//// protected/internal - Esse atributo/metodo/classes so pode ser acessado por ele mesmo e por sua classe pai
+///
+//// Exemplo de POO
 //using POO;
 
 //Carro carro1 = new Carro();
@@ -56,20 +73,65 @@
 //carros.Add(carro1); 
 //carros.Add(carro2);
 //carros.Add(carro3);
+///
+//// Exercicios 25/03 e 26/03
 
-using POO;
+//// Exercicio 01 - Crie uma classe chamada `Livro` com os seguintes atributos:
+////- `titulo` (tipo `string`)
+////- `autor` (tipo `string`)
+////- `paginas` (tipo `int`)
 
-Livro livro1  = new Livro();
-livro1.titulo = "Eu robo";
-livro1.autor = "Isaac Asimov";
-livro1.pagina = 315;
+//Livro livro1 = new Livro();
+//livro1.titulo = "Eu robo";
+//livro1.autor = "Isaac Asimov";
+//livro1.pagina = 315;
 
-List<Livro> livros = new List<Livro>();
-livros.Add(livro1);
-foreach (var item in livros)
-{
-    Console.WriteLine(item.titulo);
-    Console.WriteLine(item.autor);
-    Console.WriteLine(item.pagina);
+//Console.WriteLine(livro1.titulo);
+//Console.WriteLine(livro1.autor);
+//Console.WriteLine(livro1.pagina);
 
-}
+//// Exercicio 02 -Crie uma classe chamada `Aluno` com os seguintes atributos:
+////- `nome` (tipo `string`)
+////- `idade` (tipo `int`)
+////- `notaFinal` (tipo `double`)
+////Depois, crie dois objetos da classe `Aluno`, atribua valores a eles e exiba os detalhes de ambos.
+
+//Aluno aluno1 = new Aluno();
+//aluno1.nome = "Daniel";
+//aluno1.idade = 20;
+//aluno1.notaFinal = 6.9;
+
+//Aluno aluno2 = new Aluno();
+//aluno2.nome = "alexandryni";
+//aluno2.idade = 19;
+//aluno2.notaFinal = 6.6;
+
+//Console.WriteLine(aluno1.nome);
+//Console.WriteLine(aluno1.idade);
+//Console.WriteLine(aluno1.notaFinal);
+//Console.WriteLine(aluno2.nome);
+//Console.WriteLine(aluno2.idade);
+//Console.WriteLine(aluno2.notaFinal);
+
+//// Exercicio 03 - Crie uma classe chamada `Produto` com os seguintes atributos:
+////- `nome` (tipo `string`)
+////- `preco` (tipo `double`)
+////- `quantidadeEmEstoque` (tipo `int`)
+////Também crie o seguinte método:
+////- `CalcularTotal` que retorna o total da `quantidadeEmEstoque` *  `preco`
+////Depois, crie um objeto da classe `Produto`, altere os valores dos atributos e imprima as informações antes e depois da alteração, também imprima o resultado do método CalcularTotal.
+
+//using POO;
+
+//Produto produto1 = new Produto();
+//Console.Write("Digite o nome do produto: ");
+//produto1.nome = Console.ReadLine();
+//Console.WriteLine();
+//Console.Write($"Digite o valor do {produto1.nome}: ");
+//produto1.preco = double.Parse(Console.ReadLine());
+//Console.WriteLine();
+//Console.Write($"Digite quantos {produto1.nome} tem no estoque: ");
+//produto1.qtdEestoque = int.Parse(Console.ReadLine());
+//Console.WriteLine();
+//Console.Write($"O valor total do {produto1.nome} e: ");
+//Console.WriteLine(produto1.CalcularTotal());
